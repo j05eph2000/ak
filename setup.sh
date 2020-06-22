0cde1e9647b7b88d3d01ac5d7594add10d5de5ef
j05eph2000 <47005688+j05eph2000@users.noreply.github.com>
2020-06-22 22:01:31 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -180,8 +180,8 @@ After=network.target
 User=root
 Group=root
 Type=forking
-ExecStart=/usr/local/bin/akikcoind -daemon -conf=$CONF_DIR/$CONFIG_FILE -datadir=$CONF_DIR
-ExecStop=/usr/local/bin/akikcoin-cli -conf=$CONF_DIR/$CONFIG_FILE -datadir=$CONF_DIR stop
+ExecStart=/usr/local/bin/akikcoind -daemon -conf=$CONF_DIR/akikcoin.conf -datadir=$CONF_DIR
+ExecStop=/usr/local/bin/akikcoin-cli -conf=$CONF_DIR/akikcoin.conf -datadir=$CONF_DIR stop
 Restart=always
 PrivateTmp=true
 TimeoutStartSec=10m
@@ -194,6 +194,7 @@ EOF
   sleep 10
   systemctl start akikcoin_$ALIAS.service
   systemctl enable akikcoin_$ALIAS.service >/dev/null 2>&1
+ 
  
   rm -rf setup.sh
 
